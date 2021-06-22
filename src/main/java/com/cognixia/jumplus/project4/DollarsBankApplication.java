5287b93d0e717317c60ac0ee1f74428e2e58426f
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-06-22 11:19:22 -0400

    Initial Commit

---
+++ src/main/java/com/cognixia/jumplus/project4/DollarsBankApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jumplus.project4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollarsBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollarsBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognixia/jumplus/project4/DollarsBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognixia.jumplus.project4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollarsBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
